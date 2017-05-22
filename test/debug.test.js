fd48693bcc2c7e132737f522507e150f7b56df20
Tommaso Allevi <tommaso.allevi@buongiorno.com>
2017-05-22 16:15:25 +0200

    Fix debug.test.js

--- test/debug.test.js
+++ test/debug.test.js
@@ -9,14 +9,14 @@ var condom = require('../condom')
 download('visionmedia/debug', 'test/data/debug', function (err) {
   if (err) throw err
 
-  tap.test('express should be ok', function (t) {
+  tap.test('debug should be ok', function (t) {
     t.plan(1)
 
     var stream = condom({
-      packageJson: require(path.join(__dirname, '/data/express', 'package.json')),
+      packageJson: require(path.join(__dirname, '/data/debug', 'package.json')),
       globPattern: ['**/*.js', '!node_modules/**/*', '!test/**/*', '!tests/**/*', '!examples/**/*'],
       globOptions: {
-        cwd: path.join(__dirname, '/data/express')
+        cwd: path.join(__dirname, '/data/debug')
       }
     })
 
